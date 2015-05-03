b3c64e1ba8c1c8a79716c67cc476eb1344144d17
Daniel Otero Avalle <daniel.oteroavalle@gmail.com>
2015-05-03 19:13:06 +0200

    working on atn

--- source/src/interp/ATNFunc.java
+++ source/src/interp/ATNFunc.java
@@ -4,13 +4,14 @@ import java.util.HashMap;
 
 public class ATNFunc {
 
-	private HashMap<String,ATNTree> nodes;
+	private HashMap<String,ATNTree> node2Tree;
 	private String startingNode;
 	private ATNTree tree;
+	private String parseText;
 
 	public ATNFunc(ATNTree t) {
 
-		nodes = new HashMap<String,ATNTree>();
+		node2Tree = new HashMap<String,ATNTree>();
 
 		for (int i = 0; i < t.getChildCount(); i++) {
 			createNode(t.getChild(i));
@@ -27,11 +28,42 @@ public class ATNFunc {
 	// Stack.popArc(), Stack.pushArc();
 	public Data run(String text, Stack Stack) {
 
-		return new Data(false);
+		parseText = text;
+		boolean b = executeNode(startingNode);
+
+		return new Data(b);
+	}
+
+	private boolean executeNode(String name) {
+
+		ATNTree node = node2Tree.get(name);
+
+		if (node.getChild(1) == ATNLexer.ACCEPT) return true;
+
+		ArrayList<int> list = new ArrayList<int>();
+
+		for (int i = 1; i < node.getChildCount(); i++) {
+			ATNTree arc = node.getChild(i);
+			if (evaluateExpression(arc.getChild(0))) {
+				list.add(i);
+			}
+		}
+
+		if (!list.empty()) {
+			for (int x:list) {
+				executeListInstructions(node.getChild(x).getChild(2));
+				boolean b = executeNode(node.getChild(x).getChild(1).getText());
+				if (b) return true;
+				// desfer el executeListInstructions -> push pop activation record + copia vars globals
+			}
+		}
+
+		return false;
+
 	}
 
 	private void createNode(ATNTree t) {
-		nodes.put(t.getChild(0).getText(), t.getChild(1));
+		node2Tree.put(t.getChild(0).getText(), t.getChild(1));
 	}
 
 }
