6393baff1a5f135167a0491b76a29fd893c28022
Guido Arnau <guido.arnau@gmail.com>
2015-05-16 11:14:39 +0200

    Corrected for and added tests

--- source/src/interp/Interp.java
+++ source/src/interp/Interp.java
@@ -387,13 +387,13 @@ public class Interp {
                 return null;
 
             case ATNLexer.FOR:
-                evaluateExpression(t.getChild(0));
+                executeInstruction(t.getChild(0));
                 while (true) {
                     value = evaluateExpression(t.getChild(1));
                     checkBoolean(value);
                     if (!value.getBooleanValue()) return null;
                     Data r = executeListInstructions(t.getChild(3));
-                    evaluateExpression(t.getChild(2));
+                    executeInstruction(t.getChild(2));
                     if (r != null) return r;
                 }
 
